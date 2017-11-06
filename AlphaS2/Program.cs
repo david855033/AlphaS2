9683f40fd39c8b1ad6d7934005e9ac2c083d4ad8
Lo Yu-Cheng <david855033@gmail.com>
2017-11-06 16:37:03 +0800

    doing sql connection.

--- AlphaS2/Program.cs
+++ AlphaS2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AlphaS2
 {
     class Program
---
+++ AlphaS2/Sql.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AlphaS2
+{
+    class Sql:IDisposable
+    {
+        SqlConnection connection;
+        const string connectionStr = @"Server=localhost;Integrated security=SSPI;database=master";
+        public Sql() {
+            connection = new SqlConnection(connectionStr);
+            connection.Open();
+        }
+
+        public void Dispose() {
+            connection.Close();
+        }
+    }
+}
