9d4849106f05c42bf25a028246fe35670f563d37
Lo Yu-Cheng <david855033@gmail.com>
2017-11-07 20:05:22 +0800

    .

--- AlphaS2/Program.cs
+++ AlphaS2/Program.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data;
 
 namespace AlphaS2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Sql sql = new Sql();
-            sql.CreateTable("testTable1", new SqlField[] {
-                new SqlField("Field1","int",false)
-            });
+        static void Main(string[] args) {
+            using (Sql sql = new Sql()) {
+                sql.DropTable("testTable1");
+                sql.CreateTable("testTable1", new SqlColumn[] {
+                    new SqlColumn("f1","int",false),
+                    new SqlColumn("f2","int",false)
+                });
+                sql.AddColumn("testTable1", new SqlColumn("f3", "nchar(20)", true));
+                //sql.SetPrimaryKey("testTable1","f1");
+                SqlInsertData insertData = new SqlInsertData();
+                insertData.AddColumn("f1", SqlDbType.Int);
+                insertData.AddColumn("f2", SqlDbType.Int);
+                insertData.AddColumn("f3", SqlDbType.Char);
+                insertData.AddData(new Object[] { 1, 2, "s" });
+                insertData.AddData(new Object[] { 3, 4, 3 });
+                insertData.AddData(new Object[] { 5, 3, "你好" });
+                sql.Insert("testTable1", insertData);
+                Console.ReadKey(false);
+            }
         }
     }
 }
--- AlphaS2/Sql.cs
+++ AlphaS2/Sql.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
+using System.Data;
 namespace AlphaS2
 {
     class Sql : IDisposable
@@ -17,29 +17,165 @@ namespace AlphaS2
         private void CreateConnection() {
             connection = new SqlConnection(connectionStr);
             connection.Open();
+            connection.InfoMessage += Connection_InfoMessage;
         }
         public void Dispose() {
             connection.Close();
         }
-
-        public void CreateTable(string tableName, IEnumerable<SqlField> field) {
-            string commandStr = $"If not exists (select name from sysobjects where name = '{tableName}') CREATE TABLE {tableName}+(" +
-                $"{String.Join(",",field.Select(x=>x.GetTableCmdStr()))}";
-            Console.WriteLine(commandStr);
-            //SqlCommand myCommand = new SqlCommand(commandStr, connection);
-            //myCommand.ExecuteNonQuery();
+        string infoMessage = "";
+        void Connection_InfoMessage(object sender, SqlInfoMessageEventArgs e) {
+            infoMessage = e.Message;
         }
+
+        //如果表單不存在則建立
+        public void CreateTable(string table, IEnumerable<SqlColumn> column) {
+            try {
+                string commandStr = $@"If not exists (select name from sysobjects where name = '{table}')
+                    BEGIN CREATE TABLE {table}
+                    ({String.Join(",", column.Select(x => x.GetTableCmdStr()))})
+                    PRINT 'SUCCESS'
+                    END ELSE PRINT 'FAIL'";
+                SqlCommand sqlCommand = new SqlCommand(commandStr, connection);
+                sqlCommand.ExecuteNonQuery();
+                if (infoMessage == "SUCCESS") {
+                    Console.WriteLine($"SQL: create table: {table}, Columns({column.Count()})");
+                } else {
+                    Console.WriteLine($"SQL: {table} exists");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        //如果表單存在則刪除表單
+        public void DropTable(string table) {
+            try {
+                string commandStr = $@"If exists (select name from sysobjects where name = '{table}')
+                    BEGIN DROP TABLE {table}
+                    PRINT 'SUCCESS'
+                    END ELSE PRINT 'FAIL'";
+                SqlCommand sqlCommand = new SqlCommand(commandStr, connection);
+                sqlCommand.ExecuteNonQuery();
+                if (infoMessage == "SUCCESS") {
+                    Console.WriteLine($"SQL: drop table: {table}");
+                } else {
+                    Console.WriteLine($"SQL: {table} not exists");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public void SetPrimaryKey(string table, string column) {
+            try {
+                string commandStr = $@"IF COL_LENGTH('{table}', '{column}') IS NOT NULL
+                    BEGIN ALTER TABLE {table} ADD PRIMARY KEY({column});
+                    PRINT 'SUCCESS'
+                    END ELSE PRINT 'FAIL'";
+                SqlCommand sqlCommand = new SqlCommand(commandStr, connection);
+                sqlCommand.ExecuteNonQuery();
+                if (infoMessage == "SUCCESS") {
+                    Console.WriteLine($"SQL: Set Primary Key, table: {table}, Column: {column}");
+                } else {
+                    Console.WriteLine($"SQL: {table}-{column} not exists");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public void SetConstraintPrimaryKey(string table, string[] columns) {
+
+        }
+        public void AddColumn(string table, SqlColumn column) {
+            try {
+                string commandStr = $@"IF COL_LENGTH('{table}', '{column.name}') IS NULL
+                    BEGIN ALTER TABLE {table} ADD {column.GetTableCmdStr()}
+                    PRINT 'SUCCESS'
+                    END ELSE PRINT 'FAIL'";
+                SqlCommand sqlCommand = new SqlCommand(commandStr, connection);
+                sqlCommand.ExecuteNonQuery();
+                if (infoMessage == "SUCCESS") {
+                    Console.WriteLine($"SQL: Add Column, table: {table}, Column: {column.name}");
+                } else {
+                    Console.WriteLine($"SQL: {table}-{column.name} exists");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void DropColumn(string table, string column) {
+            try {
+                string commandStr = $@"IF COL_LENGTH('{table}', '{column}') IS NOT NULL
+                    BEGIN ALTER TABLE {table} DROP COLUMN {column}
+                    PRINT 'SUCCESS'
+                    END ELSE PRINT 'FAIL'";
+                SqlCommand sqlCommand = new SqlCommand(commandStr, connection);
+                sqlCommand.ExecuteNonQuery();
+                if (infoMessage == "SUCCESS") {
+                    Console.WriteLine($"SQL: Drop Column, table: {table}, Column: {column}");
+                } else {
+                    Console.WriteLine($"SQL: {table}-{column} not exists");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void Insert(string table, SqlInsertData insertData) {
+            try {
+                string commandStr = insertData.GetInserQuery(table);
+                SqlCommand sqlCommand = new SqlCommand(commandStr, connection);
+                insertData.AddCmdParameters(sqlCommand);
+                var ColumnList = insertData.ColumnList;
+                var dataList = insertData.dataList;
+                foreach (var row in dataList) {
+                    for (int i = 0; i < ColumnList.Count; i++) {
+                        sqlCommand.Parameters["@" + ColumnList[i].name].Value = row[i];
+                    }
+                    int affectedRow = sqlCommand.ExecuteNonQuery();
+                    Console.WriteLine($"SQL: Insert, ({affectedRow})");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+       
     }
-    class SqlField
+
+    class SqlColumn
     {
-        public string name="defaultName";
-        public string dataType="smallint";
-        public bool isNull=false;
-        public SqlField(string name, string dataType, bool isNull) {
-            this.name = name;this.dataType = dataType;this.isNull = isNull;
+        public string name = "defaultName";
+        public string type = "smallint";
+        public SqlDbType sqlDbType;
+        public bool isNull = false;
+        public SqlColumn(string name, string dataType, bool isNull) {
+            this.name = name; this.type = dataType; this.isNull = isNull;
+        }
+        public SqlColumn(string name, SqlDbType sqlDbType) {
+            this.name = name; this.sqlDbType = sqlDbType;
         }
         public string GetTableCmdStr() {
-            return name + " " + dataType + " " + (isNull ? "NULL" : "NOT NULL");
+            return name + " " + type + " " + (isNull ? "NULL" : "NOT NULL");
+        }
+    }
+
+    class SqlInsertData
+    {
+        public List<SqlColumn> ColumnList = new List<SqlColumn>();
+        public List<object[]> dataList = new List<object[]>();
+        public void AddColumn(string name, SqlDbType sqlDbType) {
+            ColumnList.Add(new SqlColumn(name, sqlDbType));
+        }
+        public string GetInserQuery(string table) {
+            return $@"INSERT INTO {table}({String.Join(",", ColumnList.Select(x => x.name))})
+                VALUES({String.Join(",", ColumnList.Select(x => "@" + x.name))})";
+        }
+        public void AddCmdParameters(SqlCommand sqlCommand) {
+            foreach (var Column in ColumnList) {
+                sqlCommand.Parameters.Add(new SqlParameter("@" + Column.name, Column.sqlDbType));
+            }
+        }
+        public void AddData(object[] add) {
+            dataList.Add(add);
         }
     }
 }
